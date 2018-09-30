24a3bacf73c09d8933edd375a7bf78bd6772bbb8
Suraj Shrestha <shrestha.suraj.2013@gmail.com>
2018-09-30 09:05:29 +0545

    Initial commit

---
+++ ItNepal.ConsoleApp/Math/Arithmetic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItNepal.ConsoleApp.Math
+{
+    class Human
+    {
+        public int numberOfEyes;
+        public Human()
+        {
+            numberOfEyes = 2;
+        }
+
+        public Human(int numberOfEyes)
+        {
+            this.numberOfEyes = numberOfEyes;
+
+        }
+
+
+        public void EyeOps(string Ops)
+
+        {
+
+            Console.WriteLine("{1}eye is {0}", Ops,this.numberOfEyes);
+        }
+    }
+}
---
+++ ItNepal.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItNepal.ConsoleApp.Math;
+
+namespace ItNepal.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            Console.WriteLine("this human has {0}", human.numberOfEyes);
+             
+           
+            
+               Human secHuman = new Human(1);
+            Console.WriteLine("{0} this human has {1}",secHuman.numberOfEyes);
+            Console.ReadKey();
+
+
+        }
+    }
+}
